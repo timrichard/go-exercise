5c549b07d82bbb94a63398864f4a58810ae9f804
Tim Richard <tim.richard@gmail.com>
2016-10-18 18:01:10 +0100

    Ex5, make friends. Tests to follow.

--- ex005_make_friends/main.go
+++ ex005_make_friends/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+)
+
+var flist = make(map[string]map[string]bool)
+
+
+func listFriends(name string) {
+	for friendName := range flist[name] {
+		fmt.Println(friendName)
+	}
+}
+
+func linkFriends(nameOne, nameTwo string) {
+	personFriendList, defined := flist[nameOne]
+
+	if (!defined) {
+		personFriendList = make(map[string]bool)
+		flist[nameOne] = personFriendList
+	}
+
+	personFriendList[nameTwo] = true
+}
+
+func makeFriends(nameOne, nameTwo string) {
+	linkFriends(nameOne, nameTwo)
+	linkFriends(nameTwo, nameOne)
+}
+
 func main() {
-	// your code here
+	makeFriends("userEight", "userTwo")
+	makeFriends("userEight", "userThree")
+	listFriends("userEight")
+	listFriends("userThree")
 }
